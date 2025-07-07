7d2b24716bef915b3dca624b8a7f9a24bb0a2e0f
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-07-06 19:41:32 -0500

    Ultimos cambios realizados para transacciones

--- src/components/atoms/transactionCard/TransactionCard.tsx
+++ src/components/atoms/transactionCard/TransactionCard.tsx
@@ -9,16 +9,32 @@ interface ReceiptCardProps {
   hora: string;
   comercio: string;
   monto: string;
+  estado: boolean | string;
+  vehiculo: string;
+  placa: string;
 }
 
 const TransactionCard: React.FC<ReceiptCardProps> = ({
+  referencia,
+  fecha,
+  hora,
+  comercio,
+  monto,
+  estado,
+  vehiculo,
+  placa,
 
 }) => {
   return (
     <div className="ticket">
       <div className="ticket_content">
 				<div className="ticket_header">
-					<p>hola</p>
+					<h1>{vehiculo} {placa}</h1>
+          <p>{comercio}</p>
+					<p>Referencia: {referencia}</p>
+					<p>{fecha} - {hora}</p>
+					<p>{monto}</p>
+					<p>{estado}</p>
 				</div>
 			</div>
     </div>
--- src/components/pages/transactions/Transactions.tsx
+++ src/components/pages/transactions/Transactions.tsx
@@ -9,8 +9,11 @@ const Transactions = () => {
         <h1>Transactions Page</h1>
       </div>
       <TransactionCard
+        vehiculo="Carro"
+        placa="ABC123"
+        estado="Aprobada"
         monto="$100.00"
-        referencia="Pago por servicios prestados"
+        referencia="xxxx-xxxx-xxxx"
         fecha="2023-10-01"
         hora="10:00 AM"
         comercio="Comercio Ejemplo"
