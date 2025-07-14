3845343ed7433868bfd1a1ba5c838fbf540f8009
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-07-14 15:51:55 -0500

    integrando de nmuevo rutas protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,16 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Login />} />
-
+        <Route element={
+          <ProtectedRoute>
+            <Header />
+          </ProtectedRoute>
+        }>
           <Route path="/clientes" element={<Clients/>} />
           <Route path='/vehiculos' element={<Vehicles/>}/>
           <Route path="/transacciones" element={<Transactions />} />
           <Route path="/home" element={<Home />} />
-
+        </Route>
       </Routes>
   )
 }
